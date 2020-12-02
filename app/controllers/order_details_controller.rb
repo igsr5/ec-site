373d4db1753f5bd11bab48c8906a1fad2269af9c
Sora Ichigo <ichigo@web-freework.com>
2020-12-02 10:49:13 +0900

    order_detail_paramを作成 期待しないフォームからの値を消す

--- app/controllers/order_details_controller.rb
+++ app/controllers/order_details_controller.rb
@@ -1,10 +1,20 @@
 class OrderDetailsController < ApplicationController
   def create
+    @orderDetail = OrderDetail.new(order_detail_param)
+    if @task.save
+      redirect_to carts_path, notice: "タスク「#{@orderDetail.title}」をカートに入れました。"
+    else
+      redirect_to product_path(@orderDetail.product_id)
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  def order_detail_param
+    params.require(:orderDetail).permit(:product_id, :product_count,:cart_id)
   end
 end
