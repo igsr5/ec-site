f23e0938a107052d7f9f54702717f2501d5e92ee
Sora Ichigo <ichigo@web-freework.com>
2020-12-02 11:15:39 +0900

    サイトアクセス時にカートを作成 applications_controller.rb before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+    before_action :set_cart_id_session
+
+    private
+
+    def set_cart_id_session
+        unless session[:cart_id]
+            cart=Cart.new
+            if cart.save
+                session[:cart_id] =cart.id
+            end
+        end
+    end
 end
