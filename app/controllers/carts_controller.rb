12db383767daed215747247c1916a5648d00701f
Sora Ichigo <ichigo@web-freework.com>
2020-12-03 15:54:48 +0900

    注文金額が表示されるようにした

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,7 +1,7 @@
 class CartsController < ApplicationController
   layout 'carts'
   def show
-    cart = Cart.find(session[:cart_id])
-    @order_details = cart.order_details
+    @cart = Cart.find(session[:cart_id])
+    @order_details = @cart.order_details
   end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,21 @@
 class Cart < ApplicationRecord
   has_many :order_details
+
+  def price_sum
+    orde_details=self.order_details
+    @price_sum=0
+
+    order_details.each do |order_detail|
+      price=order_detail.product.price * order_detail.product_count
+      @price_sum+=price
+    end
+
+    @price_sum*=1.1 #tax
+    return @price_sum.floor
+  end
+
+  def price_add_fee
+    @price_add_fee=self.price_sum()+550
+    return @price_add_fee
+  end
 end
