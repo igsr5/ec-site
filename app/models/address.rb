5bdffa8dd60db526c670d33324a2f1edbc6a818b
Sora Ichigo <ichigo@web-freework.com>
2020-12-07 19:42:37 +0900

    アドレスのバリデーションルールを追加

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,9 @@
 class Address < ApplicationRecord
+    validates :family_name, presence: true
+    validates :given_name, presence: true
+    validates :postal_code, presence: true,length: {within: 7..7}
+    validates :prefecture, presence: true
+    validates :city, presence: true
+    validates :address1, presence: true
+    validates :email, presence: true
 end
