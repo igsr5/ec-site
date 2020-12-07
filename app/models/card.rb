94bfee1b3c5357a8077246fae2169aafa72308f8
Sora Ichigo <ichigo@web-freework.com>
2020-12-07 20:27:43 +0900

    カード情報有効期限を正規表現でバリデーションするようにした

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,6 @@
 class Card < ApplicationRecord
+    validates :name, presence: true
+    validates :card_num, presence: true,length:{within: 16..16}
+    validates :expiration_date, presence: true ,length: {within: 5..5},format: {with: /\A[0-9][0-9]\/[0-9][0-9]\z/}
+    validates :security_code, presence: true,length:{within: 3..4}
 end
