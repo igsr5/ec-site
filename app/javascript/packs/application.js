dea2e1d8bdb83fee12117cc6bd3bb636cff4221d
Sora Ichigo <ichigo@web-freework.com>
2020-12-09 22:33:09 +0900

    checkouts.html.slimのJSをapplication.jsに移した

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,3 +21,21 @@
 
 import './styles/main'
 
+function numberOnly(id) {
+  var element = document.getElementById(id);
+  var regex = /[^0-9]/gi;
+  element.value = element.value.replace(regex, "");
+}
+
+function numberAndHyphenOnly(id){
+  var element = document.getElementById(id);
+  char_length=element.value.length;
+  if (char_length == 2 && !element.value.match("/")){
+      element.value+="/"
+  }
+  var regex = /[^0-9\/]/gi;
+  element.value = element.value.replace(regex, "");
+}
+
+var postal_code = document.getElementById("postal_code");
+postal_code.textContent="〒"+postal_code.textContent.slice(0,3)+"-"+postal_code.textContent.slice(3,7)
