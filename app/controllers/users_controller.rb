273add4a2bcdebe96bae62ceedaf37a9c62a69ea
Sora Ichigo <ichigo@web-freework.com>
2020-12-14 23:43:19 +0900

    ユーザー登録処理を作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,21 @@
 class UsersController < ApplicationController
   def show
-    @user=User.new
+    @users=User.all
   end
 
   def new
+    @user=User.new
+  end
+  def create
+    @user=User.create(user_params)
+    redirect_to :users if @user.valid?
   end
 
   def edit
+  end
+  private
+
+  def user_params 
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
