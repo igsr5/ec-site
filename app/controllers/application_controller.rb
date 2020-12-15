baaa499a5efbf670d3c249aed0a51f5a2b9ed8a4
Sora Ichigo <ichigo@web-freework.com>
2020-12-15 22:08:23 +0900

    ログイン情報を取得する関数current_userを作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :set_cart_id_session
+  helper_method :current_user
 
   private
+  
+  def current_user
+    @current_user ||= User.find(session[:current_user]) if session[:current_user]
+  end
 
   def set_cart_id_session
     unless session[:cart_id]
