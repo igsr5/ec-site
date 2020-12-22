fb87cf2583080b8b2fe774dca99a24d48bf6d6fe
Sora Ichigo <ichigo@web-freework.com>
2020-12-22 23:46:00 +0900

    ラジオボタンでフォームの表示・非表示を変えれるようにした

--- app/javascript/packs/users_form.js
+++ app/javascript/packs/users_form.js
@@ -2,8 +2,10 @@ $('[name="page[category]"]').change(function(){
   console.log("1");
   var radioval = $(this).val();
   if(radioval == "new"){
+    $(".form-new").removeClass("d-none");
     console.log("aa")
   }else{
+    $(".form-new").addClass("d-none");
     console.log("bb")
   }
 })
