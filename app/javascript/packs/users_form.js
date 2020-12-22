c30c496f562dda41682261650b1354403e4c185e
Sora Ichigo <ichigo@web-freework.com>
2020-12-23 00:32:55 +0900

    住所2は必須にしないように修正

--- app/javascript/packs/users_form.js
+++ app/javascript/packs/users_form.js
@@ -1,13 +1,10 @@
 $('[name="page[category]"]').change(function(){
-  console.log("1");
   var radioval = $(this).val();
   if(radioval == "new"){
     $(".form-new").removeClass("d-none");
-    $("input.form-control").attr('required',true)
-    console.log("aa")
+    $("input.required").attr('required',true)
   }else{
     $(".form-new").addClass("d-none");
-    $("input.form-control").attr('required',false)
-    console.log("bb")
+    $("input.required").attr('required',false)
   }
 })
