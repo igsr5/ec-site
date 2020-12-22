76a89b19040c87bf8b321c492c81ca767fefc187
Sora Ichigo <ichigo@web-freework.com>
2020-12-22 22:35:21 +0900

    住所新規追加ラジオボタンの値を"new"にした

--- app/javascript/packs/users_form.js
+++ app/javascript/packs/users_form.js
@@ -1,7 +1,7 @@
 $('[name="page[category]"]').change(function(){
   console.log("1");
   var radioval = $(this).val();
-  if(radioval == 1){
+  if(radioval == "new"){
     console.log("aa")
   }else{
     console.log("bb")
