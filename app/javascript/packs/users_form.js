013f2cfa889bbf9d23e2e25e59087f1583b3d258
Sora Ichigo <ichigo@web-freework.com>
2020-12-23 00:05:04 +0900

    フォームを表示したときのみ、inputタグにrequired属性がつくようにした

--- app/javascript/packs/users_form.js
+++ app/javascript/packs/users_form.js
@@ -3,9 +3,11 @@ $('[name="page[category]"]').change(function(){
   var radioval = $(this).val();
   if(radioval == "new"){
     $(".form-new").removeClass("d-none");
+    $("input.form-control").attr('required',true)
     console.log("aa")
   }else{
     $(".form-new").addClass("d-none");
+    $("input.form-control").attr('required',false)
     console.log("bb")
   }
 })
