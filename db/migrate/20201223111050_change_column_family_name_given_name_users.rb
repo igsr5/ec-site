c89165b3d9a337452cca230583e7c29bb0b7b9d8
Sora Ichigo <ichigo@web-freework.com>
2020-12-23 20:17:52 +0900

    usersテーブルのnameをfamily_name,given_nameに分けた

---
+++ db/migrate/20201223111050_change_column_family_name_given_name_users.rb
@@ -0,0 +1,6 @@
+class ChangeColumnFamilyNameGivenNameUsers < ActiveRecord::Migration[6.0]
+  def change
+    change_column_null :users, :family_name ,false
+    change_column_null :users, :given_name,false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_14_135133) do
+ActiveRecord::Schema.define(version: 2020_12_23_111050) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,11 +72,12 @@ ActiveRecord::Schema.define(version: 2020_12_14_135133) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
     t.string "email", null: false
     t.string "password_digest", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "family_name", null: false
+    t.text "given_name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
