f779517b93a960deb34941cde9ffcecdda763570
Sora Ichigo <ichigo@web-freework.com>
2020-12-23 20:32:45 +0900

    fix User validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   has_many :addresses
   has_many :receipts
   has_secure_password
-  validates :name, presence: true
+  validates :family_name, presence: true
+  validates :given_name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, unless: -> { validation_context == :hoge }
   validates :password_confirmation, presence: true, unless: -> { validation_context == :hoge }
