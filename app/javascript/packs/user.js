0ef183004fa3e2f6880a6ed7351a39292696750f
Sora Ichigo <ichigo@web-freework.com>
2020-12-27 22:18:26 +0900

    配送先編集フォームの表示処理を実装

---
+++ app/javascript/packs/user.js
@@ -0,0 +1,10 @@
+$(".edit-link").click(function(){
+  address_id=$(this).data('id');
+  $address_edit=$("#address-edit-"+address_id);
+  $address=$("#address-"+address_id);
+  console.log($address)
+  $('.address-data').show();
+  $(".edit").hide();
+  $address.hide();
+  $address_edit.show();
+})
