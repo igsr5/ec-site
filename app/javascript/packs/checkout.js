198e96eeb145e4257d0e867e3984e9f3a8e5ce02
Sora Ichigo <ichigo@web-freework.com>
2021-01-03 17:26:08 +0900

    不要なJS関数を削除

--- app/javascript/packs/checkout.js
+++ app/javascript/packs/checkout.js
@@ -1,19 +1,3 @@
-window.numberOnly=function (id) {
-  var element = document.getElementById(id);
-  var regex = /[^0-9]/gi;
-  element.value = element.value.replace(regex, "");
-}
-
-window.numberAndHyphenOnly=function (id){
-  var element = document.getElementById(id);
-  char_length=element.value.length;
-  if (char_length == 2 && !element.value.match("/")){
-      element.value+="/"
-  }
-  var regex = /[^0-9\/]/gi;
-  element.value = element.value.replace(regex, "");
-}
-
 var postal_code = document.getElementsByClassName("postal_code");
 for(var i = 0; postal_code[i] != null; i++){
   postal_code[i].textContent="〒"+postal_code[i].textContent.slice(0,3)+"-"+postal_code[i].textContent.slice(3,7);
