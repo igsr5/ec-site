7a853672080c830c0b43c6850ac09fb1b50005be
Sora Ichigo <ichigo@web-freework.com>
2021-01-08 12:15:17 +0900

    fix checkout.js

--- app/javascript/packs/checkout.js
+++ app/javascript/packs/checkout.js
@@ -3,4 +3,9 @@ for(var i = 0; postal_code[i] != null; i++){
   postal_code[i].textContent="〒"+postal_code[i].textContent.slice(0,3)+"-"+postal_code[i].textContent.slice(3,7);
 }
 
+window.numberOnly = function(id) {
+  var element = document.getElementById(id);
+  var regex = /[^0-9]/gi;
+  element.value = element.value.replace(regex, "");
+}
 
